17844bd9376946f9a37932282731da48dddbcfef
sotrteacher <lamberto9@gmail.com>
2020-03-03 13:06:43 -0600

    Marzo, 3, 2020

---
+++ hola.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc,char argv[])
+{
+  printf("HOLA MUNDO C\n");
+  printf("Teclea un int: ");
+  scanf("%d*%c",&argc);
+  printf("%d\n",argc);
+  return 0;
+}//end main()
